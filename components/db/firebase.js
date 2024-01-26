3cf825a1de0e580428ec5a0cb87b8edcdf345727
Neha <neharadialcode@gmail.com>
2024-01-26 15:18:16 +0530

    firebase.js added

---
+++ components/db/firebase.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCr41MKz1VzffM1ddAJ4F989kP5sx5IaYI",
+  authDomain: "fir-practice-33835.firebaseapp.com",
+  projectId: "fir-practice-33835",
+  storageBucket: "fir-practice-33835.appspot.com",
+  messagingSenderId: "946864864820",
+  appId: "1:946864864820:web:3adfed5c488b992816d461",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const realTimeDB = getDatabase(app);
+export const db = getAuth(app);
