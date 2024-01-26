275b209d533deaedbc76125e06c2cfbc1f069aa3
Neha <neharadialcode@gmail.com>
2024-01-26 15:12:26 +0530

    signup and login page added

---
+++ pages/login.js
@@ -0,0 +1,12 @@
+import Login from "@/components/authentication/Login";
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+};
+
+export default LoginPage;
